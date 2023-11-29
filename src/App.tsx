99bd16c0bb14cac4fc7106981dfaaa738353e8f1
bri <bcamesi@gmail.com>
2023-11-29 17:56:28 -0500

    random quotes render from quote component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import Quote from './Quote';
+import { Quote, quotes } from './Quote';
 
 function App() {
+  
+  // Manage state of current quote, null if no quote is generated
+
+  const [currentQuote, setCurrentQuote] = useState<Quote | null>(null);
+
+  // Generate a random quote from the list of quotes. It gives a random index number depending on how long the quotes array is. Then it uses that index number to get the quote from the array and set it as the current quote.
+
+  const handleRandomQuote = () => {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    const randomQuote = quotes[randomIndex];
+    setCurrentQuote(randomQuote);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Random Quote Generator</h1>
-        <Quote text="Quote 1" author="Author 1" />
+
+        {/* Checks if currentQuote is truth and renders the quote in the Quote component. */}
+
+        {currentQuote ? (
+
+          // JSX syntax for rendering a random quote
+
+          <Quote text={currentQuote.text} author={currentQuote.author} />
+        ) : (
+
+          // blank paragraph before a quote is generated
+          <p></p>
+
+        )}
+
+        {/* onclick event */}
+
+        <button onClick={handleRandomQuote}>Generate Random Quote</button>
       </header>
     </div>
   );
--- src/Quote.tsx
+++ src/Quote.tsx
@@ -1,19 +1,20 @@
 import React, { FC } from 'react';
 
-interface Quote {
-    text: string;
-    author: string;
-  }
-  
-  const quotes: Quote[] = [
-    { text: 'Quote 1', author: 'Author 1' },
-  ];
+export interface Quote {
+  text: string;
+  author: string;
+}
 
-const Quote: FC<Quote> = ({ text, author }) => (
+const quotes: Quote[] = [
+  { text: 'Quote 1', author: 'Author 1' },
+  { text: 'Quote 2', author: 'Author 2' }
+];
+
+const QuoteComponent: FC<Quote> = ({ text, author }) => (
   <div className="quote">
     <p>"{text}"</p>
     <p>- {author}</p>
   </div>
 );
 
-export default Quote;
+export { QuoteComponent as Quote, quotes };
